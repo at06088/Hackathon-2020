d20c5d953b71bba3a8d8286df6a25a5d72a32727
at06088 <46770385+at06088@users.noreply.github.com>
2020-02-29 15:13:37 -0500

    Create form.php

---
+++ form.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+
+<form action="checkme.php" method="get">
+Username <input type="text" name="usrname"><br>
+Password: <input type="text" name="pword"><br>
+<input type="submit">
+</form>
+
+    
+    
+    
+</body>
+</html>
